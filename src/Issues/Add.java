a105a666d91da14202efe1f9cff7ccc8574ce0fc
Henriette Kissling <henriette.kissling@outlook.com>
2020-12-28 10:27:04 +0100

    Added methods for outer representation. Should probably be moved to a dedicated class

--- src/Issues/Add.java
+++ src/Issues/Add.java
@@ -20,4 +20,31 @@ public class Add implements ICommand {
     public ILevel getMinimumLevel(){
         //return LevelOne;
     }
+
+    // The following methods handle the outer representation of the command -
+    // perhaps, should be moved to a dedicated class (e.g. AddRepresentation.java)
+
+    /**
+     *
+     * @return Returns the short name of the command (e.g. to be displayed on buttons)
+     */
+    public String getCommandName(){
+        return "add";
+    }
+
+    /**
+     *
+     * @return Returns a String representation of the corresponding git command to display on the command line
+     */
+    public String getGitCommand(){
+        return "git add";
+    }
+
+    /**
+     *
+     * @return Returns a short description of the command, which can be displayed to the user if necessary
+     */
+    public String getCommandDescription(){
+        return "Fügt Änderungen zur Staging-Area hinzu";
+    }
 }
