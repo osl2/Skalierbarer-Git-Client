70b6bd416f67ee3c2cba3ebb93de3402f3d77de1
Henriette Kissling <henriette.kissling@outlook.com>
2020-12-28 10:15:58 +0100

    First draft for Add. Contains a set of optional methods that interact with the current level and perform git add if allowed

--- src/Issues/Add.java
+++ src/Issues/Add.java
@@ -1,2 +1,23 @@
-public class Add {
+public class Add implements ICommand {
+
+    /**
+     * OPTIONAL
+     * performs git add if current level is greater equal than minimum required level
+     */
+    public void execute(ILevel level){
+        if (level.getLevelNumber() >= this.getMinimumLevel()){
+            //perform git add
+        }
+        else{
+            //do nothing
+        }
+    }
+
+    /**
+     * OPTIONAL
+     * @return The lowest level at which the command can be invoked
+     */
+    public ILevel getMinimumLevel(){
+        //return LevelOne;
+    }
 }
