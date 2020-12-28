47435420e4b1f4b435599cd0c115b72e8bec6c8f
Raphael <raphael.thome.rot@gmail.com>
2020-12-28 23:20:58 +0100

    First draft for pull and ICommand. Is similiar to add. Contains get and set methods for parameters.

--- src/Commands/ICommand.java
+++ src/Commands/ICommand.java
@@ -1,2 +1,10 @@
 public interface ICommand {
+
+    public void execute(ILevel level);
+    public boolean isAllowed(ILevel level);
+    public ILevel getMinimumLevel();
+    public String getCommandName();
+    public String getGitCommand();
+    public String getCommandDescription();
+
 }
