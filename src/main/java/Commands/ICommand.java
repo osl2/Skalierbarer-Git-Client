63d1a0108595fb48895408b028d7423fb2228733
Jan Hirschmann <jan.hirschmann@student.kit.edu>
2020-12-29 14:48:36 +0100

    Added JavaDoc to Checkout and ICommand

--- src/main/java/Commands/ICommand.java
+++ src/main/java/Commands/ICommand.java
@@ -4,8 +4,24 @@ import Levels.ILevel;
 
 public interface ICommand {
 
+    /**
+     * Method to execute the command
+     * @param level Competence level that is currently set
+     * @return true, if the command has been executed successfully
+     */
     boolean execute(ILevel level);
+
+    /**
+     * Method to check if a command can be executed in the level
+     * @param level Level that needs to be checked
+     * @return True if the Command can be executed in that level
+     */
     boolean isAllowed(ILevel level);
+
+    /**
+     * Method to get the minimum level necessary to execute the command
+     * @return Level that is at least necessarry to execute the command
+     */
     ILevel getMinimumLevel();
 
 }
