69b43fce62c743ac60587140ee1ba4cd781274f2
Raphael <raphael.thome.rot@gmail.com>
2020-12-29 02:52:59 +0100

    First draft of Branch and BranchDialogView. Lists of branches and commits in this class maybe should be exportet to an dataholder class. Contains corrected version of pull.

--- src/Commands/Pull.java
+++ src/Commands/Pull.java
@@ -5,65 +5,65 @@ public class Pull implements ICommand {
      * Performs git pull if allowed, does nothing otherwise
      * @param level The current level
      */
-    public void execute(ILevel level) {
-        public void execute(ILevel level){
-            if (isAllowed(level)){
-                //perform git pull
-            }
-            else{
-                //do nothing
-            }
+    public void execute(ILevel level){
+        if (isAllowed(level)){
+            //perform git pull
+        }
+        else{
+            //do nothing
+        }
     }
-        /**
-         * OPTIONAL
-         * Needed for command execution, button availability (active/ inactive) etc.
-         * @param level The current level
-         * @return Returns true, if current level is greater equal than minimum required level (command is allowed), false otherwise
-         */
-        public boolean isAllowed(ILevel level){
-            if (level.getLevelNumber() >= this.getMinimumLevel()){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        /**
-         * OPTIONAL
-         * @return The lowest level at which the command can be invoked
-         */
-        public ILevel getMinimumLevel(){
-            //return LevelThree;
-        }
 
-        // -------------------------------------------------------------------------------------------------------------
-        // The following methods handle the outer representation of the command -
-        // perhaps, should be moved to a dedicated class (e.g. AddRepresentation.java)
-        // -------------------------------------------------------------------------------------------------------------
-
-        /**
-         *
-         * @return Returns the short name of the command (e.g. to be displayed on buttons)
-         */
-        public String getCommandName(){
-            return "pull";
+    /**
+     * OPTIONAL
+     * Needed for command execution, button availability (active/ inactive) etc.
+     * @param level The current level
+     * @return Returns true, if current level is greater equal than minimum required level (command is allowed), false otherwise
+     */
+    public boolean isAllowed(ILevel level){
+        if (level.getLevelNumber() >= this.getMinimumLevel()){
+            return true;
         }
-
-        /**
-         *
-         * @return Returns a String representation of the corresponding git command to display on the command line
-         */
-        public String getGitCommand(){
-            return "git pull";
+        else{
+            return false;
         }
+    }
 
-        /**
-         *
-         * @return Returns a short description of the command, which can be displayed to the user if necessary
-         */
-        public String getCommandDescription(){
-            return "Lädt Änderungen aus dem angegebenen online-Repositories herunter und merged sie mit dem aktuellen Zweig";
-        }
+    /**
+     * OPTIONAL
+     * @return The lowest level at which the command can be invoked
+     */
+    public ILevel getMinimumLevel(){
+        //return LevelThree;
+    }
+
+    // -------------------------------------------------------------------------------------------------------------
+    // The following methods handle the outer representation of the command -
+    // perhaps, should be moved to a dedicated class (e.g. AddRepresentation.java)
+    // -------------------------------------------------------------------------------------------------------------
+
+    /**
+     *
+     * @return Returns the short name of the command (e.g. to be displayed on buttons)
+     */
+    public String getCommandName(){
+        return "pull";
+    }
+
+    /**
+     *
+     * @return Returns a String representation of the corresponding git command to display on the command line
+     */
+    public String getGitCommand(){
+        return "git pull";
+    }
+
+    /**
+     *
+     * @return Returns a short description of the command, which can be displayed to the user if necessary
+     */
+    public String getCommandDescription(){
+        return "Lädt Änderungen aus einem Online-Repo und merged sie";
     }
 
     /**
