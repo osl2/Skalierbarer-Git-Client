bac9663b0200cd8ca186952168659909ca45e188
Tobias Manske <ugnbf@student.kit.edu>
2021-01-01 14:31:46 +0100

    Create StashView, TODO: discuss IView

--- src/main/java/Views/IView.java
+++ src/main/java/Views/IView.java
@@ -4,6 +4,7 @@ public interface IView {
 
     /**
      * Opens the lower part of the main window.
+     * TODO: discuss: Besser GUI Fragment zurückgeben und die GUI das machen lassen?
      */
     public void view();
 }
---
+++ src/main/java/Views/StashView.java
@@ -0,0 +1,15 @@
+package Views;
+
+/**
+ * Stash Frame for the main GUI window.
+ *
+ * @author Tobias Manske
+ */
+public class StashView implements IView {
+    /* StashArray */
+    /*   */
+
+    public void view() {
+
+    }
+}
