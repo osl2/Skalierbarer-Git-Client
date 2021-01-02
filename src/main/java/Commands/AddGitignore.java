1ff84b67e5c86d5ddae11defa09c83a8f309521a
Henriette Kissling <henriette.kissling@outlook.com>
2021-01-02 20:26:32 +0100

    Renamed the command, removed methods for adding and removing from .gitignore and added methods from ICommand interface

---
+++ src/main/java/Commands/AddGitignore.java
@@ -0,0 +1,31 @@
+package Commands;
+
+import Git.GitBlob;
+
+import java.util.List;
+
+public class AddGitignore implements ICommand {
+
+    @Override
+    public boolean execute() {
+        return false;
+    }
+
+    @Override
+    public boolean undo() {
+        return false;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return null;
+    }
+
+    /**
+     * This method adds the files that should be ignored to the .gitignore file.
+     * @param blobs A list of GitBlobs whose paths should be added to the .gitignore 
+     * TODO: reconsider using a list of Strings instead, since the .gitignore may also contain RegExes
+     */
+    public void setBlobs(List<GitBlob> blobs){
+    }
+}
