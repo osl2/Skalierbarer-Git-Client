b848c7df9c301da0f26183e27143be31509727c0
Henriette Kissling <henriette.kissling@outlook.com>
2021-01-02 17:31:30 +0100

    Added undo and getErrorMessage method

--- src/main/java/Commands/ICommand.java
+++ src/main/java/Commands/ICommand.java
@@ -10,5 +10,18 @@ public interface ICommand {
      */
     boolean execute();
 
+    /**
+     * Method to undo execute() (if possible)
+     * @return true, if the command has been undone successfully
+     */
+    boolean undo();
+
+    /**
+     * @return error message, if the command execution was not successful
+     * @see #execute() 
+     */
+    String getErrorMessage();
+
+
 
 }
