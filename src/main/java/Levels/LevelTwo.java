37102a0fd00806bc3dbc7736f95c50ede836cde4
Jan Hirschmann <jan.hirschmann@student.kit.edu>
2021-01-07 16:00:56 +0100

    Made all levels Implement GetLevelName and GetCommands

--- src/main/java/Levels/LevelTwo.java
+++ src/main/java/Levels/LevelTwo.java
@@ -1,16 +1,25 @@
 package Levels;
 
+import Commands.ICommand;
+
+import java.util.List;
+
 public class LevelTwo implements ILevel{
 
     /**
      * Returns the name of the Level.
      * @return the name of the level.
      */
-    public String getLevelName() {return "Level 1"}
+    @Override
+    public String getLevelName() {
+        throw new AssertionError("not implemented");
+    }
 
     /**
      * Returns a list of all git commands of this level.
      * @return the list of git commands.
      */
-    public List<ICommand> getCommands() {return null;}
+    @Override
+    public List<ICommand> getCommands() {
+        throw new AssertionError("not implemented");}
 }
