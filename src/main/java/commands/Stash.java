90fe5de803b0c08501887e0770dc3c184393dc90
Tobias Manske <ugnbf@student.kit.edu>
2021-01-18 12:24:05 +0100

    Design Stash

---
+++ src/main/java/commands/Stash.java
@@ -0,0 +1,33 @@
+package commands;
+
+public class Stash implements ICommand, ICommandGUI {
+    private String stashMessage;
+
+    public boolean execute() {
+        return false;
+    }
+
+    public void setStashMessage(String stashMessage) {
+        this.stashMessage = stashMessage;
+    }
+
+    public String getErrorMessage() {
+        return null;
+    }
+
+    public String getCommandLine(String userInput) {
+        return null;
+    }
+
+    public String getName() {
+        return "Stash";
+    }
+
+    public String getDescription() {
+        return "Open Stash UI";
+    }
+
+    public void onButtonClicked() {
+
+    }
+}
