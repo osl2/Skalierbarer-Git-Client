6ec80e60d1d2ec5a79efe64e4436553602fc29dd
Henriette Kissling <henriette.kissling@outlook.com>
2021-01-21 17:33:56 +0100

    Changed implemented interface to IDialogView

--- src/main/java/dialogviews/FirstuseView.java
+++ src/main/java/dialogviews/FirstuseView.java
@@ -4,12 +4,20 @@ import views.IView;
 
 import javax.swing.*;
 
-public class FirstuseView implements IView {
+public class FirstuseView implements IDialogView {
 
     /**
      * Returns the lower part of the main window.
      */
     public JPanel getView() {
         throw new AssertionError("not Implemented yet");
+    }
+
+    public void show() {
+
+    }
+
+    public void update() {
+
     }
 }
