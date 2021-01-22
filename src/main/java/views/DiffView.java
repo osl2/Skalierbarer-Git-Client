d8217a4f1e16169604a67789b0be857673b986ad
Tobias Manske <ugnbf@student.kit.edu>
2021-01-22 20:45:40 +0100

    Fix DiffView imports and IDiffView extends JPanel

--- src/main/java/views/DiffView.java
+++ src/main/java/views/DiffView.java
@@ -1,14 +1,32 @@
 package views;
 
+import git.GitCommit;
+import git.GitFile;
+
+import javax.swing.*;
+
 public class DiffView implements IDiffView {
 
-  /**
-   * Opens the difference between the given file and and the previous version of the file.
-   *
-   * @param fileName the name of the given file.
-   */
-  public void openDiff(String fileName) {
-  }
+    /**
+     * Opens the difference between the given file and and the previous version of the file.
+     *
+     * @param fileName the name of the given file.
+     */
+    public void openDiff(String fileName) {
+    }
 
-  public void setDiff(GitCommit activeCommit, GitFile file) {return null;}
+
+    public JPanel openDiffView() {
+        return null;
+    }
+
+    /**
+     * Opens the difference between the given file and and the previous version of the file.
+     *
+     * @param activeCommit
+     * @param file
+     */
+    public void setDiff(GitCommit activeCommit, GitFile file) {
+
+    }
 }
--- src/main/java/views/IDiffView.java
+++ src/main/java/views/IDiffView.java
@@ -1,19 +1,22 @@
 package views;
 
+import git.GitCommit;
+import git.GitFile;
+
 import javax.swing.*;
 
-public interface IDiffView extends JPanel {
+public interface IDiffView {
 
   /**
    * Opens the view of the difference between a selected file and the
    * previous version of the file.
    */
-  public JPanel openDiffView();
+  JPanel openDiffView();
 
   /**
    * Opens the difference between the given file and and the previous version of the file.
    *
    * @param fileName the name of the given file.
    */
-  public void setDiff(GitCommit activeCommit, GitFile file);
+  void setDiff(GitCommit activeCommit, GitFile file);
 }
