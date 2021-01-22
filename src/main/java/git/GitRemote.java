b1e011d6b43ee803d3905eccd496414d7fefaed4
Tobias Manske <ugnbf@student.kit.edu>
2021-01-22 11:02:48 +0100

    Split Git and GitFacade

--- src/main/java/git/GitRemote.java
+++ src/main/java/git/GitRemote.java
@@ -1,18 +1,40 @@
 package git;
 
+import java.net.URL;
 import java.util.List;
 
 public class GitRemote {
-  private String url;
+  private URL url;
   private String gitUser;
   private String name;
   private List<GitBranch> branches;
 
 
   /* Is only instantiated inside the git Package */
-  GitRemote(String url, String gitUser, String name) {
+  GitRemote(URL url, String gitUser, String name) {
     this.url = url;
     this.gitUser = gitUser;
     this.name = name;
+  }
+
+  public void setUrl(URL url) {
+    this.url = url;
+  }
+
+  public void setGitUser(String gitUser) {
+    this.gitUser = gitUser;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Removes the remote from the underlying repository
+   *
+   * @return true iff successful
+   */
+  public boolean remove() {
+    return false;
   }
 }
