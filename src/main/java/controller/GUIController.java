b03a36be44014ffb3366e95e4c23f8a585eca573
Tobias Manske <ugnbf@student.kit.edu>
2021-01-31 15:23:27 +0100

    Code for MainWindow; A lot of ToDo's to resolve.

--- src/main/java/controller/GUIController.java
+++ src/main/java/controller/GUIController.java
@@ -1,14 +1,17 @@
 package controller;
 
 import dialogviews.IDialogView;
+import views.HistoryView;
 import views.IView;
+import views.MainWindow;
 
 public class GUIController {
 
     private static GUIController INSTANCE;
+    private MainWindow window;
 
     private GUIController() {
-
+        /* This class is a singleton */
     }
 
     public static GUIController getInstance() {
@@ -18,16 +21,23 @@ public class GUIController {
         return INSTANCE;
     }
 
+    public void setWindow(MainWindow window) {
+        this.window = window;
+    }
+
     /**
      * This method can be used to unload a IView which was loaded with setView.
      * Currently this method restores an instance of HistoryView
      * TODO: JavaDoc: Tags
      */
     public void restoreDefaultView() {
-
+        IView defaultView = new HistoryView();
+        this.openView(defaultView);
     }
 
     public void closeDialogView(IDialogView dialog) {
+        // If we implement the interface changes: This will be enough. Otherwise we need custom modality logic
+
 
     }
 
@@ -36,15 +46,26 @@ public class GUIController {
     }
 
     public void openDialog(IDialogView dialog) {
-
+        /* TODO: Resolve: The easiest way to resolve the modality issue would be to construct the JDialog here
+                          For that we need more stuff (Title, Size, etc.) in the IDialogView interface */
     }
 
     public void openView(IView view) {
+        // TODO: Resolve: This should not always be possible. What logic do we need to add here?
+        this.window.setView(view);
     }
 
+
+    // TODO: Resolve: Why do we have this split? Is it even necessary? Would a composition pattern make more sense here?
     public void update(IView view) {
+        throw new AssertionError("not implemented");
     }
 
     public void update(IDialogView dialog) {
+        throw new AssertionError("not implemented");
+    }
+
+    public void openMainWindow() {
+        this.window.open();
     }
 }
--- src/main/java/views/MainWindow.java
+++ src/main/java/views/MainWindow.java
@@ -1,28 +1,69 @@
 package views;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
-public class MainWindow implements  IView {
-  /* The MainWindow is NOT a view, rather a container for views, and therefore should NOT implement IView! */
-  /* TODO: This class should probably extend JFrame or the equivalent in JavaFX */
+public class MainWindow extends JFrame {
 
-  private IView loadedView;
+    private IView loadedView;
+    // Main-Panel
+    private JPanel contentPane;
+    // Panel containing our button row
+    private JPanel buttonPanel;
+    // Replacable panel
+    private JPanel viewPanel;
+    // Test-Button im Gui-Designer
+    private JButton button1;
 
-  public boolean setView(IView view) {
-    this.loadedView = view;
-    /* Set View */
-    return false;
-  }
+    public boolean setView(IView view) {
+        this.loadedView = view;
+        // New Data -> Refresh view
+        this.update();
+        return false;
+    }
 
 
-  /**
-   * Returns the lower part of the main window.
-   */
-  public JPanel getView() {
-    throw new AssertionError("not implemented yet");
-  }
+    /**
+     * Returns the lower part of the main window.
+     */
+    public JPanel getView() {
+        throw new AssertionError("not implemented yet");
+    }
 
-  public void update() {
+    @SuppressWarnings("BoundFieldAssignment")
+    public void update() {
+        if (this.loadedView != null)
+            this.viewPanel = this.loadedView.getView();
 
-  }
+        // Refresh menubar
+        createMenubar();
+    }
+
+    public void addButton(String text, String toolTip, ActionListener listener) {
+        JButton newButton = new JButton(text);
+        newButton.setToolTipText(toolTip);
+        newButton.addActionListener(listener);
+        this.buttonPanel.add(newButton);
+    }
+
+    public void open() {
+        this.update();
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setContentPane(this.contentPane);
+        this.setPreferredSize(new Dimension(1280, 720));
+        this.pack();
+        this.setVisible(true);
+    }
+
+    private void createMenubar() {
+        JMenuBar bar = new JMenuBar();
+        JMenu m1 = new JMenu("Menu 1");
+        JMenu m2 = new JMenu("Menu 2");
+
+        bar.add(m1);
+        bar.add(m2);
+        this.setJMenuBar(bar);
+    }
+
 }
