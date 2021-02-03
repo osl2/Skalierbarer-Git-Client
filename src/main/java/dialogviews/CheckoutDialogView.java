80386a0c4ef186b9cfe5daa3b44ae2bcaf238f0a
Tobias Manske <tobias.manske@mailbox.org>
2021-02-04 00:50:59 +0100

    CheckoutDialogView tree building; button logic
    Signed-off-by: Tobias Manske <tobias.manske@mailbox.org>

--- src/main/java/dialogviews/CheckoutDialogView.java
+++ src/main/java/dialogviews/CheckoutDialogView.java
@@ -1,5 +1,6 @@
 package dialogviews;
 
+import commands.Checkout;
 import controller.GUIController;
 import git.GitBranch;
 import git.GitCommit;
@@ -14,14 +15,16 @@ import java.awt.event.ActionEvent;
 
 public class CheckoutDialogView implements IDialogView {
 
+    private static final int MAX_BRANCH_DEPTH = 10;
+    private final DefaultMutableTreeNode root = new DefaultMutableTreeNode();
     private JPanel contentPane;
     private JTree tree1;
     private JButton abortButton;
+    private final GitData git;
     private JButton okButton;
-    private final DefaultMutableTreeNode root = new DefaultMutableTreeNode();
 
     public CheckoutDialogView() {
-
+        git = new GitData();
         // Todo: localize
         this.abortButton.setText("Abbrechen");
         this.okButton.setText("Ok");
@@ -29,32 +32,51 @@ public class CheckoutDialogView implements IDialogView {
         this.abortButton.addActionListener(e -> GUIController.getInstance().closeDialogView(this));
         this.okButton.addActionListener(this::OkButtonHandler);
 
-        new GitData().getBranches().forEach(b -> this.root.add(new BranchTreeNode(b)));
+        // Build branch-tree
+        git.getBranches().stream().map(this::buildBranchTree).forEach(root::add);
 
-        DefaultMutableTreeNode vegetableNode = new DefaultMutableTreeNode("Vegetables");
-        vegetableNode.add(new DefaultMutableTreeNode("Capsicum"));
-        vegetableNode.add(new DefaultMutableTreeNode("Carrot"));
-        vegetableNode.add(new DefaultMutableTreeNode("Tomato"));
-        vegetableNode.add(new DefaultMutableTreeNode("Potato"));
-
-        DefaultMutableTreeNode fruitNode = new DefaultMutableTreeNode("Fruits");
-        fruitNode.add(new DefaultMutableTreeNode("Banana"));
-        fruitNode.add(new DefaultMutableTreeNode("Mango"));
-        fruitNode.add(new DefaultMutableTreeNode("Apple"));
-        fruitNode.add(new DefaultMutableTreeNode("Grapes"));
-        fruitNode.add(new DefaultMutableTreeNode("Orange"));
-        //add the child nodes to the root node
-        this.root.add(vegetableNode);
-        this.root.add(fruitNode);
         this.tree1.setRootVisible(false);
-
-
         ((DefaultTreeModel) this.tree1.getModel()).setRoot(root);
+    }
+
+    private BranchTreeNode buildBranchTree(GitBranch b) {
+        BranchTreeNode root = new BranchTreeNode(b);
+        GitCommit c = b.getCommit();
+        for (int i = 0; i < MAX_BRANCH_DEPTH; i++) {
+            if (c.getParents().length > 1)
+                break;
+
+            CommitTreeNode node = new CommitTreeNode(c);
+            root.add(node);
+            c = c.getParents()[0];
+        }
+        return root;
     }
 
 
     private void OkButtonHandler(ActionEvent e) {
-        TreePath path = tree1.getSelectionPath();
+        TreePath selected = tree1.getSelectionPath();
+        if (selected == null) {
+            //todo: localize? GuiController?
+            JOptionPane.showMessageDialog(null,
+                    "Es muss ein Zweig / eine Einbuchung ausgewählt werden",
+                    "Fehler",
+                    JOptionPane.ERROR_MESSAGE);
+
+            return;
+        }
+        RefTreeNode node = (RefTreeNode) tree1.getSelectionPath().getLastPathComponent();
+        Checkout command = new Checkout();
+        node.configureCheckout(command);
+
+        if (command.execute()) {
+            GUIController.getInstance().closeDialogView(this);
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    "Es ist ein unerwarteter Fehler aufgetreten",
+                    "Fehler",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     /**
@@ -64,17 +86,17 @@ public class CheckoutDialogView implements IDialogView {
      */
     @Override
     public String getTitle() {
-        return "Titel";
+        return "Checkout";
     }
 
     /**
-     * The Size of the newly created Dialog
+     * The size of the Dialog
      *
      * @return 2D Dimension
      */
     @Override
     public Dimension getDimension() {
-        return new Dimension(400, 400);
+        return new Dimension(800, 600);
     }
 
     /**
@@ -88,17 +110,26 @@ public class CheckoutDialogView implements IDialogView {
     }
 
     /**
-     * method that ist called to update the dialog view.
+     * method that is called to update the dialog view.
      */
     public void update() {
 
     }
 
+
     private void createUIComponents() {
         this.tree1 = new JTree(this.root);
     }
 
-    private class CommitTreeNode extends DefaultMutableTreeNode {
+    private static abstract class RefTreeNode extends DefaultMutableTreeNode {
+
+        public RefTreeNode() {
+        }
+
+        abstract void configureCheckout(Checkout checkout);
+    }
+
+    private static class CommitTreeNode extends RefTreeNode {
         private final GitCommit commit;
 
         private CommitTreeNode(GitCommit commit) {
@@ -111,11 +142,17 @@ public class CheckoutDialogView implements IDialogView {
 
         @Override
         public String toString() {
-            return commit.getHash();
+            // todo: use api method
+            return commit.getHash().substring(commit.getHash().length() - 7) + " - " + commit.getMessage();
+        }
+
+        @Override
+        void configureCheckout(Checkout checkout) {
+            checkout.setDestination(commit);
         }
     }
 
-    private class BranchTreeNode extends DefaultMutableTreeNode {
+    private static class BranchTreeNode extends RefTreeNode {
         private final GitBranch branch;
 
         private BranchTreeNode(GitBranch branch) {
@@ -129,6 +166,11 @@ public class CheckoutDialogView implements IDialogView {
 
         public GitBranch getBranch() {
             return branch;
+        }
+
+        @Override
+        void configureCheckout(Checkout checkout) {
+            checkout.setDestination(branch);
         }
     }
 
