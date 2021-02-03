d772e69617676deb5f91716be1549235a22412d5
Tobias Manske <tobias.manske@mailbox.org>
2021-02-03 13:28:26 +0100

    Apply Styling and fix javadoc
    Signed-off-by: Tobias Manske <tobias.manske@mailbox.org>

--- src/main/java/commands/Checkout.java
+++ src/main/java/commands/Checkout.java
@@ -2,69 +2,68 @@ package commands;
 
 import git.GitBranch;
 import git.GitCommit;
-import java.util.List;
 
 public class Checkout implements ICommand, ICommandGUI {
-  private String errorMessage;
-  private String commandLine;
-  private String commandName;
-  private String commandDescription;
-  private GitBranch branch;
-  private GitCommit commit;
+    private String errorMessage;
+    private String commandLine;
+    private String commandName;
+    private String commandDescription;
+    private GitBranch branch;
+    private GitCommit commit;
 
-  /**
-   * Method to execute the command.
-   *
-   * @return true, if the command has been executed successfully
-   */
-  public boolean execute() {
-    return false;
-  }
+    /**
+     * Method to execute the command.
+     *
+     * @return true, if the command has been executed successfully
+     */
+    public boolean execute() {
+        return false;
+    }
 
 
-  public String getErrorMessage() {
-    return null;
-  }
+    public String getErrorMessage() {
+        return null;
+    }
 
-  /**
-   * Method to get the Commandline input that would be necessarry to execute the command.
-   *
-   * @param userInput Input wich the user has to make individually for executing the command
-   * @return Returns a String representation of the corresponding git command to
-   *     display on the command line
-   */
-  public String getCommandLine(String userInput) {
-    return null;
-  }
+    /**
+     * Method to get the Commandline input that would be necessary to execute the command.
+     *
+     * @param userInput Input which the user has to make individually for executing the command
+     * @return Returns a String representation of the corresponding git command to
+     * display on the command line
+     */
+    public String getCommandLine(String userInput) {
+        return null;
+    }
 
-  /**
-   * Method to get the name of the command, that could be displaied in the GUI.
-   *
-   * @return The name of the command
-   */
-  public String getName() {
-    return null;
-  }
+    /**
+     * Method to get the name of the command, that could be displayed in the GUI.
+     *
+     * @return The name of the command
+     */
+    public String getName() {
+        return null;
+    }
 
-  /**
-   * Method to get a description of the Command to describe for the user, what the command does.
-   *
-   * @return description as a String
-   */
-  public String getDescription() {
-    return null;
-  }
+    /**
+     * Method to get a description of the Command to describe for the user, what the command does.
+     *
+     * @return description as a String
+     */
+    public String getDescription() {
+        return null;
+    }
 
-  public void onButtonClicked() {
+    public void onButtonClicked() {
 
-  }
-  
-  public void setDestination(GitCommit commit) {
-    throw new AssertionError("not implemented");
-  }
+    }
 
-  public void setDestination(GitBranch branch) {
-    throw new AssertionError("not implemented");
-  }
+    public void setDestination(GitCommit commit) {
+        throw new AssertionError("not implemented");
+    }
+
+    public void setDestination(GitBranch branch) {
+        throw new AssertionError("not implemented");
+    }
 
 }
