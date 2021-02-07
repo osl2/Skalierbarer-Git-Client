70e91555c8bb625be1288f4e44c8b2538aa2a2b6
Henriette Kissling <henriette.kissling@outlook.com>
2021-02-07 13:49:41 +0100

    Modified constructor that takes errorMessage

--- src/main/java/git/exception/GitException.java
+++ src/main/java/git/exception/GitException.java
@@ -1,22 +1,13 @@
 package git.exception;
 
 public class GitException extends Exception {
-    private String errorMessage;
 
     public GitException(String errorMessage){
-        this.errorMessage = errorMessage;
+        super(errorMessage);
     }
 
     public GitException(){
 
-    }
-
-    public String getErrorMessage(){
-        return errorMessage;
-    }
-
-    public void setErrorMessage(String errorMessage){
-        this.errorMessage = errorMessage;
     }
 
 
