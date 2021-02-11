75f640a64d9f858aed848c45d027bb9806e4acf0
Daniel Ittensohn <bader.bauer@icloud.com>
2021-02-11 16:24:32 +0100

    Changed Init and Config to handle false inputs.

--- src/main/java/commands/Config.java
+++ src/main/java/commands/Config.java
@@ -1,15 +1,31 @@
 package commands;
 
-public class Config implements ICommand {
-    private String name = null;
-    private String eMail = null;
-    private String errorMessage;
+import settings.Settings;
 
+public class Config implements ICommand {
+    private String name;
+    private String eMail;
+    private String errorMessage = "";
+
+    public Config() {
+        if(Settings.getInstance().getUser() != null) {
+            name = Settings.getInstance().getUser().getName();
+            eMail = Settings.getInstance().getUser().getEmail();
+        }
+    }
     @Override
     public boolean execute() {
+        if(name == null || eMail == null) {
+            errorMessage = "Es muss ein Name und eine eMail-Adresse angegeben werden";
+            return false;
+        }
         return false;
     }
 
+    @Override
+    public String getErrorMessage() {
+        return null;
+    }
 
     public void setName(String name) {
         this.name = name;
