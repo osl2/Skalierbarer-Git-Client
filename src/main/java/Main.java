09e006b5f8ea47c87f58f2eadd1417545b12200a
Henriette Kissling <henriette.kissling@outlook.com>
2021-02-14 20:31:32 +0100

    Added functionality for branch combobox including renderer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,29 @@
 import controller.GUIController;
+import dialogviews.PushDialogView;
+import settings.Settings;
+
+import java.io.File;
+import java.util.Set;
+import dialogviews.BranchDialogView;
+import dialogviews.UsernamePasswordDialogView;
+import settings.Settings;
+
+import java.io.File;
 
 
 public class Main {
 
-    public static void main(String[] args) {
-        GUIController c = GUIController.getInstance();
-        c.openMainWindow();
-        // Load Data -> Persistency
-        // Todo: Load Plugins -> ???
-        // Apply Plugin changes
-        // Open GUI
-    }
+  public static void main(String[] args) {
+    Settings settings = Settings.getInstance();
+    settings.setActiveRepositoryPath(new File("C:/Users/henri/PSE/KampFinalProject"));
+    GUIController c = GUIController.getInstance();
+    c.openMainWindow();
+    c.openDialog(new PushDialogView());
+    // Load Data -> Persistency
+    // Todo: Load Plugins -> ???
+    // Apply Plugin changes
+    // Open GUI
+  }
 
-    public static void restart(String commandLine) {
-    }
+  public static void restart(String commandLine){}
 }
--- src/main/java/dialogviews/PushDialogView.java
+++ src/main/java/dialogviews/PushDialogView.java
@@ -2,17 +2,61 @@ package dialogviews;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.LinkedList;
+import java.util.List;
+
+import controller.GUIController;
+import git.GitBranch;
+import git.GitData;
+import git.exception.GitException;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.JGitInternalException;
 
 public class PushDialogView implements IDialogView {
 
   private JPanel contentPane;
   private JComboBox remoteComboBox;
   private JComboBox remoteBranchComboBox;
-  private JComboBox localBranchComboBox;
   private JCheckBox setUpstreamCheckbox;
-  private JButton aktualisierenButton;
   private JButton pushButton;
   private JPanel remotePanel;
+  private JButton refreshButton;
+  private JComboBox<GitBranch> localBranchComboBox;
+  private GitData gitData;
+
+  public PushDialogView() {
+    //initialize combo box with local branches
+    /*
+    localBranchComboBox.setRenderer(new BranchComboBoxRenderer());
+    gitData = new GitData();
+    List<GitBranch> localBranches;
+    try {
+      localBranches = gitData.getBranches();
+      DefaultComboBoxModel<GitBranch> localBranchComboBoxModel = new DefaultComboBoxModel();
+      localBranchComboBoxModel.addAll(localBranches);
+      localBranchComboBox.setModel(localBranchComboBoxModel);
+    } catch (GitException e) {
+      GUIController.getInstance().errorHandler(e);
+      GUIController.getInstance().closeDialogView();
+    }
+
+     */
+    DefaultComboBoxModel<GitBranch> model = new DefaultComboBoxModel<>();
+    gitData = new GitData();
+    List<GitBranch> localBranches = null;
+    try {
+      localBranches = gitData.getBranches();
+    } catch (GitException e) {
+      e.printStackTrace();
+    }
+    //TODO: wieso schlägt es bei model.addAll() fehl??????????
+    //model.addAll(localBranches);
+    for (GitBranch branch : localBranches){
+      model.addElement(branch);
+    }
+    localBranchComboBox.setModel(model);
+
+  }
 
   /**
    * DialogWindow Title
@@ -41,11 +85,29 @@ public class PushDialogView implements IDialogView {
    */
   @Override
   public JPanel getPanel() {
-    return null;
+    return this.contentPane;
   }
 
   public void update() {
-
+    //TODO
   }
 
+  private void createUIComponents() {
+    localBranchComboBox = new JComboBox<>();
+    localBranchComboBox.setRenderer(new BranchComboBoxRenderer());
+  }
+
+
+  private class BranchComboBoxRenderer extends JTextField implements ListCellRenderer<GitBranch> {
+
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends GitBranch> list, GitBranch value, int index, boolean isSelected, boolean cellHasFocus) {
+      GitBranch branch = (GitBranch) value;
+      this.setText(branch.getName());
+      return this;
+    }
+  }
 }
+
+
