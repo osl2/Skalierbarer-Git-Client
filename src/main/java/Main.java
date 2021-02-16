8da1fe3524a449e68ab063668b8c61b4fd3a9850
Raphael <raphael.thome.rot@gmail.com>
2021-02-16 21:46:08 +0100

    Added Test in main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 import controller.GUIController;
+import dialogviews.BranchDialogView;
 import dialogviews.UsernamePasswordDialogView;
+import settings.Settings;
+
+import java.io.File;
 
 public class Main {
 
   public static void main(String[] args) {
+    Settings.getInstance().setActiveRepositoryPath(new File("C:\\Users\\rapha\\IdeaProjects\\entwurf"));
     GUIController c = GUIController.getInstance();
     c.openMainWindow();
+    c.openDialog(new BranchDialogView());
     // Load Data -> Persistency
     // Todo: Load Plugins -> ???
     // Apply Plugin changes
