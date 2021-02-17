77dc431747fe34a47ed1b8832dda5e771b1f9142
Raphael <raphael.thome.rot@gmail.com>
2021-02-17 20:32:11 +0100

    Added RemoteView.form

--- src/main/java/views/RemoteView.java
+++ src/main/java/views/RemoteView.java
@@ -1,10 +1,43 @@
 package views;
 
-import javax.swing.JPanel;
+import controller.GUIController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RemoteView extends JPanel implements IView {
+  private JPanel panel1;
+  private JPanel remotePanel;
+  private JScrollPane remotePane;
+  private JList remoteList;
+  private JButton addButton;
+  private JPanel buttonPanel;
+  private JButton removeButton;
+  private JTextField textField1;
+  private JTextField textField2;
+  private JButton safeButton;
+  private JLabel nameLabel;
+  private JLabel urlLabel;
+
+  public RemoteView(){
+
+    removeButton.addActionListener(new ActionListener() {
+      /**
+       * Invoked when an action occurs.
+       *
+       * @param e the event to be processed
+       */
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        GUIController.getInstance().restoreDefaultView();
+      }
+    });
+  }
+
+
   public JPanel getView() {
-    return null;
+    return remotePanel;
   }
 
   public void update() {
