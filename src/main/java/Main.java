46499070b6b40cbd11fa1f5221beac83124b07e8
Daniel Ittensohn <bader.bauer@icloud.com>
2021-02-18 23:22:47 +0100

    Test Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
 import controller.GUIController;
+import dialogviews.PullDialogView;
+import settings.Settings;
+
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
+        Settings.getInstance().setActiveRepositoryPath(new File("D:\\CloneTestOrdener"));
         GUIController c = GUIController.getInstance();
         c.openMainWindow();
+        c.openDialog(new PullDialogView());
         // Load Data -> Persistency
         // Todo: Load Plugins -> ???
         // Apply Plugin changes
