da780dc166892652b400e98f9b11b23e2c3a1abf
Tobias Manske <tobias.manske@mailbox.org>
2021-02-18 13:47:52 +0100

    Move MergeConflictDialogView.form okButton to the left of the window
    Signed-off-by: Tobias Manske <tobias.manske@mailbox.org>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,16 @@
 import controller.GUIController;
-import dialogviews.MergeDialogView;
-import settings.Settings;
-
-import java.io.File;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Settings.getInstance().setActiveRepositoryPath(new File("/home/rad4day/Dokumente/src/fwupd"));
-    GUIController c = GUIController.getInstance();
-    c.openMainWindow();
-    c.openDialog(new MergeDialogView());
-    // Load Data -> Persistency
-    // Todo: Load Plugins -> ???
-    // Apply Plugin changes
-    // Open GUI
-  }
+    public static void main(String[] args) {
+        GUIController c = GUIController.getInstance();
+        c.openMainWindow();
+        // Load Data -> Persistency
+        // Todo: Load Plugins -> ???
+        // Apply Plugin changes
+        // Open GUI
+    }
 
-  public static void restart(String commandLine){}
+    public static void restart(String commandLine) {
+    }
 }
