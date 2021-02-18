22d2d8f55c1e8a31406cb63ce10b096be1264b08
Daniel Ittensohn <bader.bauer@icloud.com>
2021-02-19 00:22:45 +0100

    Merge branch 'feature-pull' of D:\PSE_Git_Client with conflicts.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,10 @@
 import controller.GUIController;
-import dialogviews.PullDialogView;
-import settings.Settings;
-
-import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-        Settings.getInstance().setActiveRepositoryPath(new File("D:\\CloneTestOrdener"));
         GUIController c = GUIController.getInstance();
         c.openMainWindow();
-        c.openDialog(new PullDialogView());
         // Load Data -> Persistency
         // Todo: Load Plugins -> ???
         // Apply Plugin changes
