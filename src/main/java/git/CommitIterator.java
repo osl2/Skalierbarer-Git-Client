2f80407c18bd3442cd6dcc0fd3234dd461359f81
Jan Hirschmann <jan.hirschmann@student.kit.edu>
2021-02-19 23:33:54 +0100

    JavaDocs

--- src/main/java/git/CommitIterator.java
+++ src/main/java/git/CommitIterator.java
@@ -4,9 +4,16 @@ import org.eclipse.jgit.revwalk.RevCommit;
 
 import java.util.Iterator;
 
+/**
+ * Class to iterate efficiently over parrent-commits.
+ */
 public class CommitIterator implements Iterator<GitCommit> {
     Iterator<RevCommit> revIterator;
 
+    /**
+     * Generates a CommitIterator by the RevCommit
+     * @param revIterator Iterator, that should be used
+     */
     public CommitIterator(Iterable<RevCommit> revIterator) {
         if (revIterator == null)
             this.revIterator = null;
--- src/main/java/git/GitData.java
+++ src/main/java/git/GitData.java
@@ -30,228 +30,259 @@ import java.util.stream.Collectors;
  */
 public class GitData {
 
-    private static org.eclipse.jgit.api.Git git;
-    private static Repository repository;
+  private static org.eclipse.jgit.api.Git git;
+  private static Repository repository;
 
-    public GitData() {
-        if (git == null || repository == null) {
-            initializeRepository();
-        }
+  /**
+   * Method to initialize GitData, it is accessible in the whole project.
+   */
+  public GitData() {
+    if (git == null || repository == null) {
+      initializeRepository();
+    }
+  }
+
+  /**
+   * Method to get the repository, it is only used in the git-package.
+   *
+   * @return repository.
+   */
+  static Repository getRepository() {
+    return repository;
+  }
+
+  /**
+   * Method to get the JGit-Instance.
+   *
+   * @return Instance of Jgit
+   */
+  static Git getJGit() {
+    return git;
+  }
+
+  /**
+   * Method to get the currently in Git selected author.
+   *
+   * @return author that is  currently selected in git
+   */
+  public GitAuthor getUser() {
+    StoredConfig config = getJGit().getRepository().getConfig();
+    return new GitAuthor(config.getString("user", null, "name"),
+        config.getString("user", null, "email")
+    );
+  }
+
+  /**
+   * Method to reinitialize the repository.
+   */
+  public void reinitialize() {
+    initializeRepository();
+  }
+
+  /**
+   * Returns the merge oommit message as prepared by git.
+   *
+   * @return String with commit message; NULL if MERGE_MSG does not exist in .git
+   */
+  public String getMergeCommitMessage() {
+
+    File mergeFile = new File(getRepository().getDirectory(), "MERGE_MSG");
+    if (!mergeFile.exists() || mergeFile.isDirectory()) {
+      return null;
+    }
+    try {
+      return new BufferedReader(new FileReader(mergeFile)).lines().collect(Collectors.joining(System.lineSeparator()));
+    } catch (FileNotFoundException e) {
+      return null;
     }
 
-    static Repository getRepository() {
-        return repository;
+  }
+
+  /**
+   * Method to initalize the Repoistory.
+   */
+  private void initializeRepository() {
+    Settings settings = Settings.getInstance();
+    File path = settings.getActiveRepositoryPath();
+    try {
+      FileRepositoryBuilder builder = new FileRepositoryBuilder();
+      builder.setMustExist(true);
+      if (path.getAbsolutePath().matches(".*/.git$"))
+        builder.setGitDir(path);
+      else
+        builder.setWorkTree(path);
+      //builder.setGitDir(path);
+      repository = builder.build();
+      git = Git.open(path);
+    } catch (IOException e) {
+      Logger.getGlobal().warning("Beim Öffnen des Git-Repositorys ist etwas schief gelaufen "
+          + "Fehlermeldung: " + e.getMessage());
+    }
+  }
+
+  /**
+   * Method to get the currently selected branch.
+   *
+   * @return Currently selected branch
+   * @throws IOException If something with the directory went wrong
+   */
+  public GitBranch getSelectedBranch() throws IOException {
+    String branchName = git.getRepository().getFullBranch();
+    return new GitBranch(branchName);
+  }
+
+  /**
+   * Get all commits of the current Repository.
+   *
+   * @return Array of all commits without stashes
+   */
+  public Iterator<GitCommit> getCommits() throws IOException, GitException {
+    try {
+      Iterable<RevCommit> allCommits;
+      allCommits = git.log().all().call();
+      return new CommitIterator(allCommits);
+    } catch (NoHeadException e) {
+      throw new GitException("Der Head wurde nicht gefunden"
+          + "\n Fehlermeldung: " + e.getMessage());
+    } catch (GitAPIException e) {
+      throw new GitException("Eine nicht genauer spezifizierte Fehlermeldung in Git ist aufgetreten \n"
+          + "Fehlermeldung: " + e.getMessage());
+    }
+  }
+
+  /**
+   * Get a page of commits for a branch.
+   *
+   * @param branch the Branch to get commits for
+   * @return Array of all commits without stashes
+   */
+  Iterator<GitCommit> getCommits(GitBranch branch) throws GitException, IOException {
+    try {
+      Iterable<RevCommit> allCommits;
+      allCommits = git.log().add(repository.resolve(branch.getFullName()))
+          .call();
+      return new CommitIterator(allCommits);
+    } catch (NoHeadException e) {
+      throw new GitException("Der Head wurde nicht gefunden \n"
+          + "Fehlermeldung: " + e.getMessage());
+    } catch (IncorrectObjectTypeException e) {
+      throw new GitException("Mit Git ist etwas Schiefgelaufen \n"
+          + "Fehlermeldung: " + e.getMessage());
+    } catch (AmbiguousObjectException | MissingObjectException e) {
+      throw new GitException("Mit den internen Objekten ist etwas schief gelaufen \n"
+          + "Fehlermeldung: " + e.getMessage());
+    } catch (GitAPIException e) {
+      throw new GitException("Mit Git ist etwas nicht genauer spezifiziertes schief gelaufen \n"
+          + "Fehlermeldung: " + e.getMessage());
+    } catch (NullPointerException e) {
+      // Apparently JGIT throws a NPE if a branch does not have a commit to reference.
+      // e.g. after initializing a repository.
+      return new CommitIterator(null);
+    }
+  }
+
+
+  /**
+   * Get all stashes of the current Repository.
+   *
+   * @return A list of all Stashes
+   */
+  public List<GitStash> getStashes() {
+    try {
+      StashListCommand stashListCommand = new StashListCommand(repository);
+      Collection<RevCommit> listOfStashes = stashListCommand.call();
+      List<GitStash> gitStashes = new ArrayList<>();
+      for (RevCommit stash : listOfStashes) {
+        gitStashes.add(new GitStash(stash));
+      }
+      return gitStashes;
+    } catch (GitAPIException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+
+  /**
+   * Get the current status of the working directory.
+   * Holds information about all active files and their current stages
+   *
+   * @return The singleton status object
+   */
+  public GitStatus getStatus() {
+    return GitStatus.getInstance();
+  }
+
+  /**
+   * Get a list of all configured remotes.
+   *
+   * @return A list of all remotes
+   */
+  public List<GitRemote> getRemotes() {
+    try {
+      List<RemoteConfig> remotes = git.remoteList().call();
+      List<GitRemote> gitRemotes = new ArrayList<>();
+      for (RemoteConfig config : remotes) {
+        List<URIish> uris = config.getURIs();
+        String user = uris.iterator().next().getUser();
+        String name = config.getName();
+        String urlString = uris.iterator().next().getPath();
+        URIish uri = config.getURIs().iterator().next();
+        gitRemotes.add(new GitRemote(uri.toString(), user, name));
+      }
+      return gitRemotes;
+    } catch (GitAPIException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  /**
+   * Method to get a list of the branches, witch are available in the current repository.
+   *
+   * @return A list of branches in the repository
+   */
+  public List<GitBranch> getBranches() throws GitException {
+    try {
+      List<Ref> branches = git.branchList().call();
+      List<GitBranch> gitBranches = new ArrayList<>();
+      for (Ref branch : branches) {
+        gitBranches.add(new GitBranch(branch));
+      }
+      return gitBranches;
+    } catch (GitAPIException e) {
+      throw new GitException("Mit Git ist etwas schief gelaufen"
+          + "Fehlermeldung: " + e.getMessage());
+    }
+  }
+
+  /**
+   * Method to get list of branches, which are available in the specific online repository.
+   *
+   * @param remote Online repository, where the branches come from
+   * @return List of branches in the repository
+   */
+  public List<GitBranch> getBranches(GitRemote remote) throws GitException {
+    Collection<Ref> refs;
+    List<GitBranch> branches = new ArrayList<GitBranch>();
+
+    try {
+      refs = Git.lsRemoteRepository()
+          .setHeads(true)
+          .setRemote(remote.getUrl().toString())
+          .setCredentialsProvider(CredentialProviderHolder.getInstance().getProvider())
+          .call();
+      for (Ref ref : refs) {
+        branches.add(new GitBranch(ref));
+      }
+    } catch (GitAPIException e) {
+      throw new GitException();
     }
 
-    static Git getJGit() {
-        return git;
-    }
+    //Collections.sort(branches);
 
-    public GitAuthor getUser() {
-        StoredConfig config = getJGit().getRepository().getConfig();
-        return new GitAuthor(config.getString("user", null, "name"),
-                config.getString("user", null, "email")
-        );
-    }
-
-    public void reinitialize() {
-        initializeRepository();
-    }
-
-    /**
-     * Returns the merge oommit message as prepared by git
-     *
-     * @return String with commit message; NULL if MERGE_MSG does not exist in .git
-     */
-    public String getMergeCommitMessage() {
-
-        File mergeFile = new File(getRepository().getDirectory(), "MERGE_MSG");
-        if (!mergeFile.exists() || mergeFile.isDirectory()) {
-            return null;
-        }
-        try {
-            return new BufferedReader(new FileReader(mergeFile)).lines().collect(Collectors.joining(System.lineSeparator()));
-        } catch (FileNotFoundException e) {
-            return null;
-        }
-
-    }
-
-    private void initializeRepository() {
-        Settings settings = Settings.getInstance();
-        File path = settings.getActiveRepositoryPath();
-        try {
-            FileRepositoryBuilder builder = new FileRepositoryBuilder();
-            builder.setMustExist(true);
-            if (path.getAbsolutePath().matches(".*/.git$"))
-                builder.setGitDir(path);
-            else
-                builder.setWorkTree(path);
-            //builder.setGitDir(path);
-            repository = builder.build();
-            git = Git.open(path);
-        } catch (IOException e) {
-            Logger.getGlobal().warning("Beim Öffnen des Git-Repositorys ist etwas schief gelaufen " +
-                    "Fehlermeldung: " + e.getMessage());
-        }
-    }
-
-    public GitBranch getSelectedBranch() throws IOException {
-        String branchName = git.getRepository().getFullBranch();
-        return new GitBranch(branchName);
-    }
-
-    /**
-     * Get all commits of the current Repository.
-     *
-     * @return Array of all commits without stashes
-     */
-    public Iterator<GitCommit> getCommits() throws IOException, GitException {
-        try {
-            Iterable<RevCommit> allCommits;
-            allCommits = git.log().all().call();
-            return new CommitIterator(allCommits);
-        } catch (NoHeadException e) {
-            throw new GitException("Der Head wurde nicht gefunden" +
-                    "\n Fehlermeldung: " + e.getMessage());
-        } catch (GitAPIException e) {
-            throw new GitException("Eine nicht genauer spezifizierte Fehlermeldung in Git ist aufgetreten \n" +
-                    "Fehlermeldung: " + e.getMessage());
-        }
-    }
-
-    /**
-     * Get a page of commits for a branch
-     *
-     * @param branch the Branch to get commits for
-     * @return Array of all commits without stashes
-     */
-    Iterator<GitCommit> getCommits(GitBranch branch) throws GitException, IOException {
-        try {
-            Iterable<RevCommit> allCommits;
-            allCommits = git.log().add(repository.resolve(branch.getFullName()))
-                    .call();
-            return new CommitIterator(allCommits);
-        } catch (NoHeadException e) {
-            throw new GitException("Der Head wurde nicht gefunden \n" +
-                    "Fehlermeldung: " + e.getMessage());
-        } catch (IncorrectObjectTypeException e) {
-            throw new GitException("Mit Git ist etwas Schiefgelaufen \n" +
-                    "Fehlermeldung: " + e.getMessage());
-        } catch (AmbiguousObjectException | MissingObjectException e) {
-            throw new GitException("Mit den internen Objekten ist etwas schief gelaufen \n" +
-                    "Fehlermeldung: " + e.getMessage());
-        } catch (GitAPIException e) {
-            throw new GitException("Mit Git ist etwas nicht genauer spezifiziertes schief gelaufen \n" +
-                    "Fehlermeldung: " + e.getMessage());
-        } catch (NullPointerException e) {
-            // Apparently JGIT throws a NPE if a branch does not have a commit to reference. e.g. after initializing a repository.
-            return new CommitIterator(null);
-        }
-    }
-
-
-    /**
-     * Get all stashes of the current Repository.
-     *
-     * @return A list of all Stashes
-     */
-    public List<GitStash> getStashes() {
-        try {
-            StashListCommand stashListCommand = new StashListCommand(repository);
-            Collection<RevCommit> listOfStashes = stashListCommand.call();
-            List<GitStash> gitStashes = new ArrayList<>();
-            for (RevCommit stash : listOfStashes) {
-                gitStashes.add(new GitStash(stash));
-            }
-            return gitStashes;
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-
-    /**
-     * Get the current status of the working directory.
-     * Holds information about all active files and their current stages
-     *
-     * @return The singleton status object
-     */
-    public GitStatus getStatus() {
-        return GitStatus.getInstance();
-    }
-
-    /**
-     * Get a list of all configured remotes.
-     *
-     * @return A list of all remotes
-     */
-    public List<GitRemote> getRemotes() {
-        try {
-            List<RemoteConfig> remotes = git.remoteList().call();
-            List<GitRemote> gitRemotes = new ArrayList<>();
-            for (RemoteConfig config : remotes) {
-                List<URIish> uris = config.getURIs();
-                String user = uris.iterator().next().getUser();
-                String name = config.getName();
-                String urlString = uris.iterator().next().getPath();
-                URIish uri = config.getURIs().iterator().next();
-                gitRemotes.add(new GitRemote(uri.toString(), user, name));
-            }
-            return gitRemotes;
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * Method to get a list of the branches, witch are available in the current repository.
-     *
-     * @return A list of branches in the repository
-     */
-    public List<GitBranch> getBranches() throws GitException {
-        try {
-            List<Ref> branches = git.branchList().call();
-            List<GitBranch> gitBranches = new ArrayList<>();
-            for (Ref branch : branches) {
-                gitBranches.add(new GitBranch(branch));
-            }
-            return gitBranches;
-        } catch (GitAPIException e) {
-            throw new GitException("Mit Git ist etwas schief gelaufen" +
-                    "Fehlermeldung: " + e.getMessage());
-        }
-    }
-
-    /**
-     * Method to get list of branches, which are available in the specific online repository
-     *
-     * @param remote Online repository, where the branches come from
-     * @return List of branches in the repository
-     */
-    public List<GitBranch> getBranches(GitRemote remote) throws GitException {
-        Collection<Ref> refs;
-        List<GitBranch> branches = new ArrayList<GitBranch>();
-
-        try {
-            refs = Git.lsRemoteRepository()
-                    .setHeads(true)
-                    .setRemote(remote.getUrl().toString())
-                    .setCredentialsProvider(CredentialProviderHolder.getInstance().getProvider())
-                    .call();
-            for (Ref ref : refs) {
-                branches.add(new GitBranch(ref));
-            }
-        } catch (GitAPIException e) {
-            throw new GitException();
-        }
-
-        //Collections.sort(branches);
-
-        return branches;
-    }
+    return branches;
+  }
 
 }
 
