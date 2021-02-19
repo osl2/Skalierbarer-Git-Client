fcf03b21cf0e1fd505d7be7273cac0337ed012af
Henriette Kissling <uzvrz@student.kit.edu>
2021-02-19 12:46:07 +0100

    Removed gitData instantiation from constructor

--- src/main/java/commands/Add.java
+++ src/main/java/commands/Add.java
@@ -18,8 +18,6 @@ public class Add implements ICommand, ICommandGUI {
   private GitData gitData;
 
   public Add(){
-    gitData = new GitData();
-    gitStatus = gitData.getStatus();
     files = new LinkedList<>();
   }
 
@@ -32,7 +30,8 @@ public class Add implements ICommand, ICommandGUI {
    */
   public boolean execute() {
     List<GitFile> stagedFiles = new LinkedList<>();
-
+    gitData = new GitData();
+    gitStatus = gitData.getStatus();
 
       //get all the staged files
     try {
