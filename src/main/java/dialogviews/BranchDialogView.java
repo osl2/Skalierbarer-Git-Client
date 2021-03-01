605235898da2064bf75fef2c9485d2f3e922c851
Raphael <raphael.thome.rot@gmail.com>
2021-03-01 13:45:01 +0100

    Now the BDV shows the the commits of the actual branch if the BDV is opened. Fixed warnings and refactored Method to load commits in the comboboxes.

--- src/main/java/dialogviews/BranchDialogView.java
+++ src/main/java/dialogviews/BranchDialogView.java
@@ -1,5 +1,4 @@
 package dialogviews;
-
 import commands.Branch;
 import controller.GUIController;
 import git.GitBranch;
@@ -16,33 +15,40 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Class of the DialogView from the command branch
+ */
 public class BranchDialogView implements IDialogView {
     private List<GitBranch> branches;
-    private LinkedList<GitCommit> commits = new LinkedList<GitCommit>();
+    private final LinkedList<GitCommit> commits = new LinkedList<>();
     private JPanel panel1;
     private JPanel BranchPanel;
-    private JComboBox branchComboBox;
-    private JComboBox commitComboBox;
+    private JComboBox<String> branchComboBox;
+    private JComboBox<String> commitComboBox;
     private JTextField nameField;
     private JButton branchButton;
     private JLabel branchLabel;
     private JLabel commitLabel;
     private JLabel nameLabel;
-    private GitData gitData = new GitData();
 
-
+    /**
+     * Constructor for a new Instance
+     */
     public BranchDialogView() {
         commitComboBox.setRenderer(new BranchDialogRenderer());
         commitComboBox.setMaximumRowCount(6);
+        // Adds the existing branches to the comboBox
         try {
+            GitData gitData = new GitData();
             branches = gitData.getBranches();
         } catch (GitException e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < branches.size(); i++) {
-            branchComboBox.addItem(branches.get(i).getName());
+        for (GitBranch branch : branches) {
+            branchComboBox.addItem(branch.getName());
         }
-
+        // Load the commits of the actual Branch in the commit-Combobox
+        reloadCommits();
 
         branchComboBox.addActionListener(new ActionListener() {
             /**
@@ -52,26 +58,7 @@ public class BranchDialogView implements IDialogView {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                int index = branchComboBox.getSelectedIndex();
-                Iterator<GitCommit> it = null;
-                GitBranch actualBranch = branches.get(index);
-                commits.clear();
-                commitComboBox.removeAllItems();
-                try {
-                    it = actualBranch.getCommits();
-                } catch (GitException gitException) {
-                    GUIController.getInstance().errorHandler(gitException);
-                } catch (IOException ioException) {
-                    GUIController.getInstance().errorHandler(ioException);
-                }
-                int count = 0;
-                while (it.hasNext()) {
-                    commits.add(count, it.next());
-                    count++;
-                }
-                for (int i = 0; i < commits.size(); i++) {
-                    commitComboBox.addItem(commits.get(i).getHashAbbrev() + " - " + commits.get(i).getMessage());
-                }
+               reloadCommits();
             }
         });
         branchButton.addActionListener(new ActionListener() {
@@ -82,13 +69,16 @@ public class BranchDialogView implements IDialogView {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
+                // checks if a commit is selected
                 if (commitComboBox.getSelectedItem() == null) {
                     GUIController.getInstance().errorHandler("Es muss ein Commit ausgewählt werden. Beim Auswählen eines" +
                             "Branches wird standartmäßig der letzte Commit ausgewählt");
                     return;
                 }
+                // Gets the commit from the combobox an the Name of the new Branch frpm the textfield
                 GitCommit commitForOp = commits.get(commitComboBox.getSelectedIndex());
                 String nameOfBranch = nameField.getText();
+                // Checks if there is a name
                 if (nameOfBranch.compareTo("") == 0) {
                     GUIController.getInstance().errorHandler("Es muss ein Name eingegeben werden");
                     return;
@@ -96,6 +86,7 @@ public class BranchDialogView implements IDialogView {
                 Branch branch = new Branch();
                 branch.setBranchPoint(commitForOp);
                 branch.setBranchName(nameOfBranch);
+                // Executes the command and closes the dialogview and sets the command line if the command was succesfull
                 if (branch.execute()) {
                     GUIController.getInstance().setCommandLine(branch.getCommandLine());
                     GUIController.getInstance().closeDialogView();
@@ -138,22 +129,33 @@ public class BranchDialogView implements IDialogView {
 
     }
 
+    /**
+     * ListCellrenderer for the commitCombobox
+     */
     private static class BranchDialogRenderer extends JTextArea implements ListCellRenderer {
-        private int minRows;
-
+        /**
+         * Constructor for a new instance
+         */
         public BranchDialogRenderer() {
             this.setLineWrap(true);
             this.setWrapStyleWord(true);
         }
 
+        /**
+         * Method to get the rendered component
+         * @param list The list of the normal Components
+         * @param value the value of the component
+         * @param index the index of the component
+         * @param isSelected if the component is selected
+         * @param hasFocus if the component has focus
+         * @return the new Component
+         */
         @Override
         public Component getListCellRendererComponent(final JList list,
                                                       final Object value, final int index, final boolean isSelected,
                                                       final boolean hasFocus) {
             Color background = Color.WHITE;
             this.setText((String) value);
-            // Only the first 6 lines of the commit message should be shown;
-            //this.setRows(minRows);
             int width = list.getWidth();
             if (isSelected) {
                 // This color is light blue.
@@ -161,12 +163,34 @@ public class BranchDialogView implements IDialogView {
             }
             this.setBackground(background);
             // this is just to activate the JTextAreas internal sizing mechanism
-            //if (width > 0) {
             this.setSize(width, Short.MAX_VALUE);
-            //}
             this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
             return this;
 
+        }
+    }
+
+    /**
+     * Method to reload the commits
+     */
+    private void reloadCommits(){
+        int index = branchComboBox.getSelectedIndex();
+        Iterator<GitCommit> it = null;
+        GitBranch actualBranch = branches.get(index);
+        commits.clear();
+        commitComboBox.removeAllItems();
+        try {
+            it = actualBranch.getCommits();
+        } catch (GitException | IOException gitException) {
+            GUIController.getInstance().errorHandler(gitException);
+        }
+        int count = 0;
+        while (it.hasNext()) {
+            commits.add(count, it.next());
+            count++;
+        }
+        for (GitCommit commit : commits) {
+            commitComboBox.addItem(commit.getHashAbbrev() + " - " + commit.getMessage());
         }
     }
 }
