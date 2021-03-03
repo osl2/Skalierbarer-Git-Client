e03f4cc4b6e83f097402702708b0ed15a335fcb1
Jan Hirschmann <jan.hirschmann@student.kit.edu>
2021-03-03 20:01:35 +0100

    Tried to fix deleting files on Windows

--- src/test/java/git/AbstractGitTest.java
+++ src/test/java/git/AbstractGitTest.java
@@ -28,9 +28,23 @@ public abstract class AbstractGitTest {
     protected Repository repository;
     protected Settings settings;
 
+    private boolean deleteDir (File dir){
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+
+        return dir.delete(); // The directory is empty now and can be deleted.
+    }
+
     @BeforeEach
     protected void beforeEach() throws GitAPIException, IOException, GitException, URISyntaxException {
-        FileUtils.deleteDirectory(repo);
+        deleteDir(repo);
         FileUtils.forceMkdir(repo);
         setupRepo();
 
