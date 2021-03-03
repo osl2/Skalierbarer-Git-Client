f0587ee620525bc92044b0be9c375259d49e7814
Daniel Ittensohn <bader.bauer@icloud.com>
2021-03-03 23:06:36 +0100

    Finish the Branch test class.

--- src/test/java/commands/BranchTest.java
+++ src/test/java/commands/BranchTest.java
@@ -1,17 +1,39 @@
 package commands;
 
+import controller.GUIController;
 import git.AbstractGitTest;
 import git.GitBranch;
 import git.GitCommit;
 import git.exception.GitException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import util.GUIControllerTestable;
 
+import java.io.IOException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
 class BranchTest extends AbstractGitTest {
+
+  static GUIControllerTestable guiControllerTestable;
+  static MockedStatic<GUIController> mockedController;
+
+  @BeforeAll
+  static void setup() {
+    guiControllerTestable = new GUIControllerTestable();
+    mockedController = mockStatic(GUIController.class);
+    mockedController.when(GUIController::getInstance).thenReturn(guiControllerTestable);
+    guiControllerTestable.resetTestStatus();
+  }
+
+  @AfterAll
+  static void tearDown() {
+    mockedController.close();
+  }
 
   @Test
   void testExecute() throws GitException {
@@ -24,13 +46,49 @@ class BranchTest extends AbstractGitTest {
     assertTrue(branchCommand.execute());
     List<GitBranch> gitBranches = gitData.getBranches();
     int countBranches = 0;
-    for (int i = 0; i < gitBranches.size(); i++) {
-      if (gitBranches.get(i).getName().compareTo("master") == 0) {
+    for (GitBranch gitBranch : gitBranches) {
+      if (gitBranch.getName().compareTo("master") == 0) {
         countBranches++;
-      } else if (gitBranches.get(i).getName().compareTo("New") == 0) {
+      } else if (gitBranch.getName().compareTo("New") == 0) {
         countBranches++;
       }
     }
+    // Check if both branches exist.
     assertEquals(2, countBranches);
+    String commandLine = branchCommand.getCommandLine();
+    assertTrue(commandLine.startsWith("git branch"));
+    // Check if the commandLine contains the name of the new Branch.
+    assertTrue(commandLine.contains(name));
+  }
+
+  @Test
+  void failTestWithNoCommitSet() {
+    Branch branch = new Branch();
+    assertFalse(branch.execute());
+    // Test if commandLine is null if execution fails.
+    assertNull(branch.getCommandLine());
+  }
+
+  @Test
+  void testExistingGetters() {
+    Branch branch = new Branch();
+    assertNotNull(branch.getName());
+    assertNotNull(branch.getDescription());
+    assertNull(branch.getCommandLine());
+    branch.onButtonClicked();
+    assertTrue(guiControllerTestable.openDialogCalled);
+  }
+
+  @Test
+  void testNotValidBranchName() throws IOException {
+    Branch branch = new Branch();
+    GitBranch master = gitData.getSelectedBranch();
+    GitCommit commit = master.getCommit();
+    String notValidName = "N   ha   ?";
+    branch.setBranchName(notValidName);
+    branch.setBranchPoint(commit);
+    assertFalse(branch.execute());
+    // Check if Exception was shown to the user.
+    assertTrue(guiControllerTestable.errorHandlerECalled);
   }
 }
