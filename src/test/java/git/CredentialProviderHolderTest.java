ab6c863ac060fccf1d07bd768dc11f0a6ec3d994
Jan Hirschmann <jan.hirschmann@student.kit.edu>
2021-03-03 11:59:55 +0100

    Added Mockito-Dependency and some tests for CredentialProvider

--- src/test/java/git/CredentialProviderHolderTest.java
+++ src/test/java/git/CredentialProviderHolderTest.java
@@ -3,13 +3,25 @@ package git;
 import controller.GUIController;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import controller.GUIController;
+import dialogviews.UsernamePasswordDialogView;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import util.GUIControllerTestable;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import java.awt.event.WindowListener;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mockStatic;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 
 class CredentialProviderHolderTest {
 
@@ -46,15 +58,15 @@ class CredentialProviderHolderTest {
   }
 
   @Test
-  void setUsernameTest() {
-    int hash = credentialProviderHolder.getProvider().hashCode();
+  public void setUsernameTest() {
+    assertEquals(new UsernamePasswordCredentialsProvider("username", "password").hashCode(), credentialProviderHolder.getProvider().hashCode());
     credentialProviderHolder.setUsername("newUsername");
-    assertNotEquals(hash, credentialProviderHolder.getProvider().hashCode());
+    assertEquals(new UsernamePasswordCredentialsProvider("newUsername", "Password").hashCode(), credentialProviderHolder.getProvider().hashCode());
   }
 
   @Test
-  void setActiveTest() {
-    assertFalse(credentialProviderHolder.isActive());
+  public void setActiveTest() {
+    assertEquals(false, credentialProviderHolder.isActive());
     credentialProviderHolder.setActive(true);
     assertTrue(credentialProviderHolder.isActive());
 
