e1ff58e2e8ea697c68a3db55c8bc6bdc930790f6
Jan Hirschmann <jan.hirschmann@student.kit.edu>
2021-03-04 12:47:46 +0100

    Some Tests for GitAuthor

--- src/test/java/git/GitAuthorTest.java
+++ src/test/java/git/GitAuthorTest.java
@@ -4,38 +4,49 @@ import org.eclipse.jgit.api.Git;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 public class GitAuthorTest {
   GitAuthor author;
 
+
+
   @Before
-  void before (){
+  public void before (){
     author = new GitAuthor("name", "email");
   }
 
   @Test
-  void equalsTest (){
+  public void equalsTest (){
     GitAuthor author2 = new GitAuthor("name", "email");
     assertTrue (author.equals(author2));
+    assertTrue(author2.equals(author));
+    assertTrue(author2.equals(author2));
+    assertTrue(author.equals(author));
+    author2 = new GitAuthor("name1", "email");
+    assertFalse(author.equals(author2));
+    assertFalse(author2.equals(author));
+    int i = new Random().ints().findFirst().getAsInt();
+    assertFalse(author.equals(i));
   }
 
   @Test
-  void hashCodeTest (){
+  public void hashCodeTest (){
     GitAuthor author2 = new GitAuthor("name", "email");
     assertEquals(author.hashCode(), author2.hashCode());
   }
 
   @Test
-  void emailTest (){
+  public void emailTest (){
     assertEquals("email", author.getEmail());
     author.setEmail("newMail");
     assertEquals("newMail", author.getEmail());
   }
 
   @Test
-  void nameTest (){
+  public void nameTest (){
     assertEquals("name", author.getName());
     author.setName("newName");
     assertEquals("newName", author.getName());
