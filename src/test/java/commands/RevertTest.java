2957203dc559d3cc2eeb04837bc15d072a748687
Daniel Ittensohn <bader.bauer@icloud.com>
2021-03-10 20:05:39 +0100

    Add some coverage.

--- src/test/java/commands/RevertTest.java
+++ src/test/java/commands/RevertTest.java
@@ -1,12 +1,17 @@
 package commands;
 
 import git.GitCommit;
+import git.GitData;
 import git.exception.GitException;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
 
 public class RevertTest extends AbstractCommandTest {
 
@@ -28,6 +33,42 @@ public class RevertTest extends AbstractCommandTest {
     assertNotNull(revert.getDescription());
     revert.onButtonClicked();
     assertTrue(guiControllerTestable.openDialogCalled);
+  }
+
+  @Test
+  void openFails() {
+    MockedConstruction<GitData> gitDataMockedConstruction = mockConstruction(GitData.class, (mock, context) -> {
+      when(mock.getBranches()).thenReturn(new ArrayList<>());
+    });
+    Revert revert = new Revert();
+    revert.onButtonClicked();
+    assertFalse(guiControllerTestable.openDialogCalled);
+    gitDataMockedConstruction.close();
+  }
+
+  @Test
+  void openFailsBecauseException() {
+    MockedConstruction<GitData> gitDataMockedConstruction = mockConstruction(GitData.class, (mock, context) -> {
+      when(mock.getBranches()).thenThrow(new GitException(""));
+    });
+    Revert revert = new Revert();
+    revert.onButtonClicked();
+    assertTrue(guiControllerTestable.errorHandlerECalled);
+    gitDataMockedConstruction.close();
+  }
+
+  @Test
+  void testExceptionHandling() throws IOException {
+    MockedConstruction<GitCommit> gitCommitMockedConstruction = mockConstruction(GitCommit.class, (mock, context) -> {
+      when(mock.revert()).thenThrow(new GitException(""));
+    });
+    GitData data = new GitData();
+    GitCommit commit = data.getSelectedBranch().getCommit();
+    Revert revert = new Revert();
+    revert.setChosenCommit(commit);
+    assertFalse(revert.execute());
+    assertTrue(guiControllerTestable.errorHandlerECalled);
+    gitCommitMockedConstruction.close();
   }
 
   @Test
--- src/test/java/dialogviews/RevertDialogViewTest.java
+++ src/test/java/dialogviews/RevertDialogViewTest.java
@@ -1,6 +1,7 @@
 package dialogviews;
 
 import commands.AbstractCommandTest;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +9,7 @@ import javax.swing.*;
 import javax.swing.tree.TreePath;
 import java.awt.event.ActionEvent;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RevertDialogViewTest extends AbstractCommandTest {
   RevertDialogView rDV;
@@ -44,5 +44,33 @@ public class RevertDialogViewTest extends AbstractCommandTest {
     rDV.update();
     assertNotNull(rDV.getDimension());
     assertNotNull(rDV.getTitle());
+  }
+
+  @Test
+  void testLoadMoreNode() throws GitAPIException {
+    generateCommits(71);
+    rDV = new RevertDialogView();
+    JPanel panel = rDV.getPanel();
+    revertTree = (JTree) FindComponents.getChildByName(panel, "revertTree");
+    assertNotNull(revertTree);
+    Object rootNode = revertTree.getModel().getRoot();
+    Object masterBranchNode = revertTree.getModel().getChild(rootNode, 0);
+    assertNotNull(masterBranchNode);
+    Object testLoadMoreNode = revertTree.getModel().getChild(masterBranchNode, revertTree.getModel().getChildCount(masterBranchNode) - 1);
+    assertNotNull(testLoadMoreNode);
+    TreePath path = new TreePath(testLoadMoreNode);
+    revertTree.setSelectionPath(path);
+    Object lastNode = revertTree.getModel().getChild(masterBranchNode, revertTree.getModel().getChildCount(masterBranchNode) - 1);
+    assertNotEquals(lastNode, testLoadMoreNode);
+  }
+
+  private void generateCommits(int amount) throws GitAPIException {
+    for (int i = 0; i < amount; i++) {
+      git.commit()
+              .setCommitter("Author " + i, i + "@example.com")
+              .setMessage("New Commit " + i)
+              .setSign(false)
+              .call();
+    }
   }
 }
